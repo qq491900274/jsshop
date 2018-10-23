59cab8b84a76a6e1ff8ec83b78f1724bb1c572be
[qq491900274] <491900274@qq.com>
2018-10-23 15:03:29 +0800

    添加登陆功能

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -7,33 +7,29 @@ use think\Session;
 
 use \think\Db;
 
-
 class Login extends Controller
 {	
 	public function __construct(){
 		parent::__construct();
 	}
-	
+	   //登陆页面
     public function index(){
-    	$this->assign('name','123');
-    	return $this->fetch('index'); 
+    	return $this->fetch('login'); 
     }
-    public function Login_in()
-    {
-      $this->LoginModel =  new \app\index\model\LoginModel();
-      $result = $this->LoginModel->hello('helloword');
-      var_dump($result['12']);
-    }
-
+    
     //验证密码功能
-     public function getpwd(){
-      $request = request()->post();
+     function getpwd(){
+        $request = request()->post();
+       
+        $this->LoginModel =  new \app\admin\model\LoginModel();
+        $val=$this->LoginModel->select('SHOP_USER','USER',"USER='{$request['user']}' AND PASSWORD='{$request['pwd']}'");
 
-      if ($request['user']=='1' && $request['pwd']=='2') {
-        return array('msg'=>'登陆成功！','state'=>'1');  
-      }else{
-        return array('msg'=>'登陆失败！','state'=>'2');
-      }
-
+        if ($val) {
+          //登陆成功保存session
+          $_SESSION['admin']=$val[0]['USER'];
+          return array('msg'=>'登陆成功！','state'=>'1');  
+        }else{
+          return array('msg'=>'登陆失败！','state'=>'2');
+        }
     }
 }
