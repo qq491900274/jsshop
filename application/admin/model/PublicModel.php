e6eae23098ffe8a250ce366f4a38ae1adf56a0c1
[qq491900274] <491900274@qq.com>
2018-10-25 15:40:44 +0800

    通用model

---
+++ application/admin/model/PublicModel.php
@@ -0,0 +1,22 @@
+<?php 
+namespace app\admin\model;
+
+use think\Model;
+use \think\Db;
+class PublicModel extends Model
+{
+
+    function __construct(){
+    	 //Db::connect('mysql://root:root@127.0.0.1:3306/test#utf8');	
+    }
+    
+    function select($table,$key,$where){
+    	if (!empty($where)) {
+    		$where=" where ".$where;
+    	}
+
+    	$sql="select {$key} from {$table} {$where}";
+    	//echo $sql;
+	 return Db::query($sql);
+    }
+}
