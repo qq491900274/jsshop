64564bfd78c357d9536ac03c89110cf6c64d4552
[qq491900274] <491900274@qq.com>
2018-10-26 15:49:57 +0800

    添加校区列表增删改查， 与教师增删该查功能

--- application/admin/controller/Lesson.php
+++ application/admin/controller/Lesson.php
@@ -41,6 +41,42 @@ class Lesson extends Controller
       //返回校区数据
       return $result;
     }
+    //删除教师
+    public function get_teacher(){
+      $request = request()->post();
+      $isok=DB::table('SHOP_TEACHER')
+            ->where('ID',$request['id'])
+            ->delete();
+
+      if($isok){
+        return '1';
+      }
+    }
+    //添加修改教师
+    public function get_teacher(){
+      $request = request()->post();
+      
+      $data=['NAME'=>$request['name'],
+            'CODE'=>$request['code'],
+            'SCHOOL'=>$request['school'],
+            'SCHOOL_ADDRESS'=>$request['school_address'],
+            'INTRO'=>$request['intro'],
+            'PIC'=>$request['pic'],
+            'DATE'=>$request['date']
+            ];
+      if (empty($request['id'])) {
+        $isok=DB::table('SHOP_TEACHER')
+            ->where('ID',$request['id'])
+            ->update($data);
+      }else{
+        $isok=DB::table('SHOP_TEACHER')
+            ->insert($data);
+      }
+
+      if($isok){
+        return '1';
+      }
+    }
     //返回教师列表页的校区数据
     public function get_school(){
       $request = request()->post();
@@ -52,7 +88,7 @@ class Lesson extends Controller
         return '';exit;
       }
 
-      $result = $this->pmodel->select('SHOP_TEACHER',"ID,NAME",$where);
+      $result = $this->pmodel->select('SHOP_SCHOOL',"ID,NAME",$where);
       return $result;
     }
     
@@ -61,9 +97,66 @@ class Lesson extends Controller
     {
       return $this->fetch('Lesson_school');
     }
+    //返回校区列表
+    public function get_schoollist(){
+      $minpage = 0;
+      $maxpage = 0;
+      $request = request()->post();
+      $this->pmodel =  new \app\admin\model\PublicModel();
+
+      //获取post当前页数。与查询条件。
+      $maxpage = empty($request['page'])?'20':20*$request['page']-1;
+      $minpage = $maxpage-20;
+      
+      //获取查询条件
+      $where = "ID!='' LIMIT {$minpage},{$maxpage}";
+      $key = "ID,PROVINCE,CITY,AREA,SCHOOL_NAME,PHONE,ADMIN_NAME";
+      $result = $this->pmodel->select('SHOP_SCHOOL',$key,$where);
+
+      //返回校区数据
+      return $result;
+    }
+
+    //添加修改校区 
+    public function update_school(){
+      $request = request()->post();
+      
+      $sql=['PROVINCE'=>$request['name'],
+            'CITY'=>$request['code'],
+            'AREA'=>$request['school'],
+            'SCHOOL_NAME'=>$request['school_address'],
+            'PHONE'=>$request['intro'],
+            'ADMIN_NAME'=>$request['pic']
+            ];
+
+      if (empty($request['id'])) {
+        $isok=DB::table('SHOP_SCHOOL')
+            ->where('ID',$request['id'])
+            ->update($sql);
+      }else{
+        $isok=DB::table('SHOP_SCHOOL')->insert($sql);
+      }
+
+      if($isok){
+        return '1';
+      }
+    }
+    //删除校区
+    public function dele_school(){
+      $request = request()->post();
+      $isok=DB::table('SHOP_SCHOOL')
+            ->where('ID',$request['id'])
+            ->delete();
+
+      if($isok){
+        return '1';
+      }
+    }
+
     //课程列表页面
     public function Lesson_list()
     {
+
       return $this->fetch('Lesson_list');
     }
     //修改课程页
