d5384e88d84044131c39bc56420366e1fba48e6d
[qq491900274] <491900274@qq.com>
2018-11-27 16:45:58 +0800

    订单列表功能

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -15,11 +15,61 @@ class Order extends Controller
 	}
 		
     public function order_list(){
-    	$request = request()->post();
-    	//判断是否
-    	if(empty($request['list'])){
-    		return $this->fetch('order_list');
-    	}
+	  $request = request()->post();
+	  //判断是否
+ 	  $request['page']=empty($request['page'])?'1':$request['page'];
+
+      $this->pmodel =  new \app\admin\model\PublicModel();
+      $where=" ifnull(STATE,0) != '1'";
+      if(!empty($request['where']['orderCode'])){
+        $where.=" and ID='{$request['where']['orderCode']}' and ";
+      }
+      if(!empty($request['where']['name'])){
+        $where.=" and ID='{$request['where']['name']}' and ";
+      }
+      if(!empty($request['where']['phone'])){
+        $where.=" and ID='{$request['where']['phone']}' and ";
+      }
+
+      //获取查询条件
+      $page=$this->return_page($request['page']);
+      
+      $where1 =$where. " LIMIT {$page['min']},{$page['max']}";
+
+      $key = "O.ID,O.CODE,O.PAYCODE,O.PRICE,O.DATETIME,O.PAYTIME,O.STATE,".
+             "U.NAME,U.PHONE,C.NAME,C.PRICE CPRICE";
+      $table=" SHOP_ORDER O LEFT JOIN SHOP_USER U ON U.ID=O.USERID".
+             " LEFT JOIN SHOP_CURRICULUM C ON C.ID=O.CURRICULUMID";
+
+      $result['value'] = $this->pmodel->select($table,$key,$where1);
+      
+      //返回总页数
+      $count=$this->pmodel->select('SHOP_ORDER','count(ID) num ',$where);
+      if(!empty($count)){
+         $result['allCount'] = ceil( $count[0]['num']/ 20);
+      }
+
+      //返回校区数据
+      return $result;
+    }
+
+    public function delete_order(){
+      $request = request()->post();
+      //判断是否
+      if(empty($request['id'])){
+        $data=['STATE'=>'1'];
+        $isok=DB::table('SHOP_TEACHER')
+          ->where('ID',$request['id'])
+          ->update($data);
+        return 1;
+      }
+      
+    }
+    public function return_page($request){
+        //获取post当前页数。与查询条件。
+        $page['max'] = empty($request)?'19':20*$request-1;
+        $page['min'] = $page['max']-19;
+        return $page;
     }
    
 }
