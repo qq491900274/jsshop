db7deb944e5568034982802e6fc905e609ae860b
[qq491900274] <491900274@qq.com>
2018-11-28 15:05:55 +0800

    1

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -16,7 +16,7 @@ class User extends Controller
   function userList(){
     $request = request()->post();
     //判断是否只请求页面
-    if(empty($request['list'])){
+    if(empty($request['where']['list'])){
        return $this->fetch('userlist');
     }
 
