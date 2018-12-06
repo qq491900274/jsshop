89878d8c5344c53fb4b1b3fa5f01bc42f9135304
[qq491900274] <491900274@qq.com>
2018-12-06 15:59:23 +0800

    添加首页显示功能

---
+++ application/index/model/PublicModel.php
@@ -0,0 +1,33 @@
+<?php 
+namespace app\index\model;
+
+use think\Model;
+use \think\Db;
+class PublicModel extends Model
+{
+
+    public function __construct(){
+    	
+    }
+    
+    public function select($table,$key,$where=''){
+    	if (!empty($where)) {
+    		$where=" where ".$where;
+    	}
+
+    	$sql="select {$key} from {$table} {$where}";
+    	
+	    return Db::query($sql);
+    }
+
+    public function dele($table,$where=''){
+        if (!empty($where)) {
+            $where=" where ".$where;
+        }
+
+        $sql="delete from {$table} {$where}";
+        
+     return Db::query($sql);
+    }
+
+}
