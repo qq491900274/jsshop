b3bb4a1bb9b0a7aba44817f8ed82a58108e116c0
[qq491900274] <491900274@qq.com>
2018-12-14 10:40:34 +0800

    1

---
+++ application/index/controller/Lesson.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\index\controller;
+use think\mobile_controller;
+use think\View;
+use think\Request;
+use think\Session;
+
+use \think\Db;
+
+
+class Lesson extends mobile_controller
+{	
+	public function __construct(){
+		parent::__construct();
+	}
+	
+    public function index(){
+    	return $this->fetch('lesson'); 
+    }
+   
+}
