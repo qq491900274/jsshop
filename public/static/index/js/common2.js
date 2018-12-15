ebb714103751c2b62dbab8b9826034d8ab3ac2bb
htb123 <2277023613@qq.com>
2018-12-15 18:16:54 +0800

    1

--- public/static/index/js/common2.js
+++ public/static/index/js/common2.js
@@ -45,6 +45,9 @@ $(function(){
 		$('.selectInfor input').change(function(){
 			$('.selectInfor').animate({height:'0px'},200);
 		});
+		$('.ensure').click(function(){
+			$('.selectInfor').animate({height:'0px'},200);
+		})
 		// 选择性别
 		$('.sex ul li').each(function(){
 			var val=$(this).find('span').html();
@@ -122,92 +125,6 @@ $(function(){
 		});
 
 
-//==================================全选与取消全选===============================================
-		//找到id为qx的input,为其绑定单击事件为function：
-		
-		window.onload=function(){
-			var chbAll=$("#qx");
-			chbAll.onclick=function(){
-				//获得cartBox下ul下所有第一个li中的input,保存在chbs
-				var chbs=document.querySelectorAll(".cartBox ul li input");
-				//console.log(chbs);
-				//遍历chbs中每个chb
-				for(var i=0;i<chbs.length;i++){
-				  //chbs数组中当前chb的checked等于事件中当前chb的checked
-				  chbs[i].checked=this.checked;
-				}
-				cilci_all_money();
-			}
-		
-
-			//找到cartBox下ul下所有第一个li中的input,保存在chbs
-			var chbs=document.querySelectorAll(".cartBox ul li input");
-			//遍历chbs
-			for(var i=0;i<chbs.length;i++){
-				chbs[i].onclick=check;//为当前chb绑定点击事件为check
-			}
-			//定义函数check
-			function check(){
-				//如果当前chb未选中
-				if(!this.checked){
-					chbAll.checked=false;//chbAll也改为未选中
-				}else{
-					//获得cartBox下ul下所有第一个li中的input,保存在chbs
-					var chbs=document.querySelectorAll(".cartBox ul li input:not(:checked)");
-					//如果chbs的length等于0
-					if(chbs.length==0){chbAll.checked=true;}//chbAll也改为选中
-					//否则chbAll改为未选中
-					else{chbAll.checked=false;}
-				}
-			}
-		}
-
-
-	
-
-			//===============================加减的实现===================================//
-			$('.num').click(function(){
-				//a.preventDefault();
-				var num=$(this).siblings('span').html();
-				if($(this).attr('val')=='1'){
-					num--;
-					if(num<=1){
-						num=1;
-					}
-					// if(num==1){
-					// 	num=1;
-					// }else{
-					// 	num--;
-					// }
-				}else{
-					num++;
-				}
-				var count=$(this).siblings('span').html(num);
-				//重新计算价格
-				cilci_all_money();
-			});
-
-		
-	//===============================勾选时========================================//
-		$('.son_check').click(function (){
-			//选中课时计算价格
-			cilci_all_money();
-		});
-		//计算所选课时价格
-		function cilci_all_money(){
-			var money=0;//总价
-			 $('.son_check').each(function(){
-				if($(this).is(':checked')){
-					var price=$(this).parents('li').find('.price1').html();
-					var num=$(this).parents('li').find('.right').children('span').html();
-					//console.log(num);
-					money+=parseFloat(price*num);
-				}
-			});
-			$('.result').find('#allmoney').html(money.toFixed(2));
-			$('.result').find('#allmoney2').html(money.toFixed(2));
-		}
-//**************************************** 购物车 end **********************************************************
 
 		//================================== 订单详情 order_detail ===================================================
 		
@@ -257,6 +174,90 @@ $(function(){
 })
 
 
+			//===============================加减的实现===================================//
+			$('.num').click(function(){
+				//a.preventDefault();
+				var num=$(this).siblings('span').html();
+				if($(this).attr('val')=='1'){
+					num--;
+					if(num<=1){
+						num=1;
+					}
+					// if(num==1){
+					// 	num=1;
+					// }else{
+					// 	num--;
+					// }
+				}else{
+					num++;
+				}
+				var count=$(this).siblings('span').html(num);
+				//重新计算价格
+				cilci_all_money();
+			});
+
+		
+	//===============================勾选时========================================//
+		$('.son_check').click(function (){
+			//选中课时计算价格
+			cilci_all_money();
+		});
+		//计算所选课时价格
+		function cilci_all_money(){
+			var money=0;//总价
+			 $('.son_check').each(function(){
+				if($(this).is(':checked')){
+					var price=$(this).parents('li').find('.price1').html();
+					var num=$(this).parents('li').find('.right').children('span').html();
+					//console.log(num);
+					money+=parseFloat(price*num);
+				}
+			});
+			$('.result').find('#allmoney').html(money.toFixed(2));
+			$('.result').find('#allmoney2').html(money.toFixed(2));
+		}
+
+//==================================全选与取消全选===============================================
+		//找到id为qx的input,为其绑定单击事件为function：
+		
+		window.onload=function(){
+			var chbAll=document.getElementById('qx');
+			chbAll.onclick=function(){
+				//获得cartBox下ul下所有第一个li中的input,保存在chbs
+				var chbs=document.querySelectorAll(".cartBox ul li input");
+				//console.log(chbs);
+				//遍历chbs中每个chb
+				for(var i=0;i<chbs.length;i++){
+				  //chbs数组中当前chb的checked等于事件中当前chb的checked
+				  chbs[i].checked=this.checked;
+				}
+				cilci_all_money();
+			}
+		
+
+			//找到cartBox下ul下所有第一个li中的input,保存在chbs
+			var chbs=document.querySelectorAll(".cartBox ul li input");
+			//遍历chbs
+			for(var i=0;i<chbs.length;i++){
+				chbs[i].onclick=check;//为当前chb绑定点击事件为check
+			}
+			//定义函数check
+			function check(){
+				//如果当前chb未选中
+				if(!this.checked){
+					chbAll.checked=false;//chbAll也改为未选中
+				}else{
+					//获得cartBox下ul下所有第一个li中的input,保存在chbs
+					var chbs=document.querySelectorAll(".cartBox ul li input:not(:checked)");
+					//如果chbs的length等于0
+					if(chbs.length==0){chbAll.checked=true;}//chbAll也改为选中
+					//否则chbAll改为未选中
+					else{chbAll.checked=false;}
+				}
+			}
+		}
+
+//**************************************** 购物车 end **********************************************************
 	 	
 
 
