53b84d646e95d474219d943c2f1b89dadc78a9c9
[qq491900274] <491900274@qq.com>
2018-12-20 15:15:41 +0800

    1

--- application/index/controller/Lesson.php
+++ application/index/controller/Lesson.php
@@ -15,6 +15,33 @@ class Lesson extends mobile_controller
 	}
 	
     public function index(){
+    	$Request=request()->post();
+		$this->pmodel =  new \app\index\model\PublicModel(); 
+
+		if ($Request['list']=='1') {
+	        $where .= "TYPE='1'";
+
+	        $key = "ID,NAME,SUBJECTID";
+	        $result['value'] = $this->pmodel->select('SHOP_SUBJECT',$key,$where);
+
+	        if (!empty($request['id'])) {
+	            foreach ($result['value'] as $key => $value) {
+	                if (empty($value['SUBJECTID'])) {
+	               		 continue;
+	                }
+
+	          		$where1=str_replace(',', "','", $value['SUBJECTID']);
+	          		$where1=" ID in ('{$where1}')";
+	          		$result['value'][$key]['SUBJECTID']=$this->pmodel->select('SHOP_SUBJECT','ID,NAME',$where1);
+
+	          		if (empty($subjectid)) {
+	            		continue;
+	          		}
+	            }
+	        }
+			return $result;
+		}
+
     	return $this->fetch('lesson'); 
     }
    
