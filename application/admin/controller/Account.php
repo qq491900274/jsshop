e1b7a00f6f8f0c92c7ab9d7dd2cb248cf4b9ce17
[qq491900274] <491900274@qq.com>
2019-01-03 15:48:29 +0800

    1

--- application/admin/controller/Account.php
+++ application/admin/controller/Account.php
@@ -13,9 +13,45 @@ class Account extends Controller
 		parent::__construct();
 	}
 
- public function index()
+ 	public function index()
     {
-      return $this->fetch('account');
+    	$request=request()->post();
+    	if (!empty($Request['list']) && $Request['list']=='1') {
+	      $where='1=1';
+
+	      if (!empty($Request['id']) && $Request=='1 ') {
+	        $where.=" AND ID='{$Request['id']}'";
+	      }
+	      
+	      //获取post当前页数。与查询条件。
+	      $maxpage = empty($request['page'])?'19':20*$request['page']-1;
+	      $minpage = $maxpage-19;
+	      
+	      $count=$this->pmodel->select('SHOP_ACCOUNT ','count(ID) num',$where)[0]['num'];
+	      //获取查询条件
+	      $where .= " LIMIT {$minpage},{$maxpage}";
+	      
+	      $key = "ID,NAME,COUPONURL,PRICE,ISWHERE,WHEREPRICE,MAXNUM,COUNT,STARTTIME,ENDTIME,CONTENT,DATETIME,PIC";
+	      $result['value'] = $this->pmodel->select('SHOP_ACCOUNT',$key,$where);
+	      $result['page'] = empty($request['page']) ? '1' : $request['page'];
+	      
+	      $result['allCount'] = ceil($count/20);
+	      //返回校区数据
+	      return $result;
+	    }
+    	return $this->fetch('account');
+    }
+
+    public function delaccount(){
+    	$Request=request()->post();
+
+    	if (empty($where['id'])) {
+    		echo '未获取到id'；
+    	}
+
+    	$where['ID']=$Request['id'];
+    	Db::table('SHOP_ACCOUNT')->where($where)->delete();
+    	return 1;
     }
  
 }
