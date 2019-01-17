f158f349807ffa16199eb23d37ea8624567afba1
[qq491900274] <491900274@qq.com>
2019-01-17 17:09:36 +0800

    1

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -31,7 +31,7 @@ class Order extends Controller
     			' LEFT JOIN SHOP_TEACHER T ON T.ID=CU.TEACHERGUID '.
     			' LEFT JOIN SHOP_SCHOOL S ON S.ID=CU.SCHOOLID';
     	$key='C.PRICE,C.ID CARTID,C.NUM,CU.NAME,CU.IMG,T.NAME TEACHERNAME,S.SCHOOL_NAME';
-    	$where=" C.USERID='{$request['id']}'";
+    	$where=" C.USERID='{$request['userid']}'";
     	$data['shop']=$this->pmodel->select($table,$key,$where);
 
     	//返回用户信息
