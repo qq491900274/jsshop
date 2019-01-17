52333ed53a5fd0ef62cc9d68bf07b199a06c2853
[qq491900274] <491900274@qq.com>
2019-01-17 17:05:06 +0800

    1

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -27,9 +27,11 @@ class Order extends Controller
     	}
 
     	$where['USERID']=$request['userid'];
-    	$table='SHOP_CART C LEFT JOIN SHOP_CURRICULUM CU ON CU.ID=C.CURRICULUMID';
-    	$key='C.PRICE,C.ID CARTID,C.NUM,CU.NAME,CU.IMG';
-    	$where=" USERID='{$request['userid']}'";
+    	$table='SHOP_CART C LEFT JOIN SHOP_CURRICULUM CU ON CU.ID=C.CURRICULUMID'.
+    			' LEFT JOIN SHOP_TEACHER T ON T.ID=CU.TEACHERGUID '.
+    			' LEFT JOIN SHOP_SCHOOL S ON S.ID=CU.SCHOOLID';
+    	$key='C.PRICE,C.ID CARTID,C.NUM,CU.NAME,CU.IMG,T.NAME TEACHERNAME,S.SCHOOL_NAME';
+    	$where=" C.USERID='{$Request['id']}'";
     	$data['shop']=$this->pmodel->select($table,$key,$where);
 
     	//返回用户信息
