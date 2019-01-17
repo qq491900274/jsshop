a9fdbf1e6c234696d020fdc9c4324b95e8fa671f
[qq491900274] <491900274@qq.com>
2019-01-17 15:07:29 +0800

    1

--- application/index/controller/Shopcart.php
+++ application/index/controller/Shopcart.php
@@ -22,8 +22,8 @@ class Shopcart extends mobile_controller
 			return $this->fetch('cart'); exit();	
 		}
     	$table='SHOP_CART C LEFT JOIN SHOP_CURRICULUM CU ON CU.ID=C.CURRICULUMID'.
-    			' LEFT JOIN SHOP_TEACHER T ON T.GUID=CU.TEACHERGUID '.
-    			' LEFT JOIN SHOP_SCHOOL S ON S.GUID=CU.SCHOOLID';
+    			' LEFT JOIN SHOP_TEACHER T ON T.ID=CU.TEACHERGUID '.
+    			' LEFT JOIN SHOP_SCHOOL S ON S.ID=CU.SCHOOLID';
     	$key='C.PRICE,C.ID CARTID,C.NUM,CU.NAME,CU.IMG,T.NAME TEACHERNAME,S.SCHOOL_NAME';
     	$where=" C.USERID='{$Request['id']}'";
     	return $this->pmodel->select($table,$key,$where);
