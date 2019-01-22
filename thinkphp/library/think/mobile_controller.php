d67c89390d94778b82f3ba395bec08cd14220d8d
[qq491900274] <491900274@qq.com>
2019-01-22 10:27:18 +0800

    1

--- thinkphp/library/think/mobile_controller.php
+++ thinkphp/library/think/mobile_controller.php
@@ -227,4 +227,21 @@ class mobile_Controller
 
         return true;
     }
+
+    protected function http_curl($url, $data = null){
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+        if (!empty($data)){
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        return $output;
+    }
 }
