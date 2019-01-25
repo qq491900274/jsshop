c0f970785823dae172dcd44304eba01c0eb8987b
[qq491900274] <491900274@qq.com>
2019-01-25 10:48:47 +0800

    1

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -13,31 +13,7 @@ class Order extends Controller
 	public function __construct(){
 		parent::__construct();
 	}
-	
-   
-    //返回购物车商品
-    public function get_orderinfo(){
-    	$request=request()->post();
-    	$this->pmodel =  new \app\index\model\PublicModel(); 
 
-    	if (empty($request['userid'])) {
-    		return '未接受id！';
-    	}
-
-    	$table='SHOP_CART C LEFT JOIN SHOP_CURRICULUM CU ON CU.ID=C.CURRICULUMID'.
-    			' LEFT JOIN SHOP_TEACHER T ON T.ID=CU.TEACHERGUID '.
-    			' LEFT JOIN SHOP_SCHOOL S ON S.ID=CU.SCHOOLID';
-    	$key='C.PRICE,C.ID CARTID,C.NUM,CU.NAME,CU.IMG,T.NAME TEACHERNAME,S.SCHOOL_NAME';
-    	$where=" C.USERID='{$request['userid']}'";
-    	$data['shop']=$this->pmodel->select($table,$key,$where);
-
-    	//返回用户信息
-        $data['userinfo']= Db::table('SHOP_USERS')
-                ->where('ID',$request['userid'])
-                ->select();
-
-        return $data;
-    }
     //提交订单
     public function suborder(){
     	$request=request()->post();
@@ -46,7 +22,6 @@ class Order extends Controller
             return $this->fetch('payment');
         }	
         
-    
     }
     
     //取消订单
