2ff1340914d4281e059ee5d911e368ab2d4c1ab3
htb123 <2277023613@qq.com>
2019-02-22 11:24:48 +0800

    1

---
+++ application/index/lib/CLogFileHandler.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\index\lib;
+interface ILogHandler
+{
+	public function write($msg);
+	
+}
+
+class CLogFileHandler implements ILogHandler
+{
+	private $handle = null;
+	
+	public function __construct($file = '')
+	{
+		$this->handle = fopen($file,'a');
+	}
+	
+	public function write($msg)
+	{
+		fwrite($this->handle, $msg, 4096);
+	}
+	
+	public function __destruct()
+	{
+		fclose($this->handle);
+	}
+}
