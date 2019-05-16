fc89e89d46917d68745a8a6f7e7cac2f0967793c
[qq491900274] <491900274@qq.com>
2019-05-16 15:24:00 +0800

    1

--- application/pc/controller/Index.php
+++ application/pc/controller/Index.php
@@ -1,25 +1,22 @@
 <?php
 namespace app\pc\controller;
-use think\Controller;
+use think\pc_controller;
 use think\View;
 use think\Request;
 use think\Session;
 
 
 
-class Index extends  PC_Controller
+class Index extends  pc_controller
 {	
 	public function __construct(){
 		parent::__construct();
 		
-		//判断用户是否登录
-        // if(empty($_SESSION['user'])){
-        // 	$this->error('请登录！',"HTTP://{$_SERVER['SERVER_NAME']}/tp?s=index/Login/index");
-        // }
 	}
 		
     public function index(){
       $this->fetch('index');
     }
     
+  
 }
