d1622b99a622d3972b722e7956120f81f52e96a2
[qq491900274] <491900274@qq.com>
2019-05-17 16:34:52 +0800

    1

---
+++ application/admin/controller/Pcconfig.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+use think\View;
+use think\Request;
+use think\Session;
+
+use \think\Db;
+
+class PcConfig extends Controller
+{ 
+  public function __construct(){
+    parent::__construct();
+
+  }
+  
+    public function index(){
+      return $this->fetch('index');
+    }
+   
+}
+ 
