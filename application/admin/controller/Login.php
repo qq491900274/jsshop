eebb7ca9715416a092363b80bfd6772d8f017f92
ZhuangWeiphp <weizhuang_ok@126.com>
2019-05-21 13:13:06 +0800

    登陆session修改，权限模块

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -22,21 +22,29 @@ class Login extends Controller
         $request = request()->post();
        
         $this->LoginModel =  new \app\admin\model\LoginModel();
-        $val=$this->LoginModel->select('SHOP_USER','USER',"USER='{$request['user']}' AND PASSWORD='{$request['pwd']}'");
-
-        if ($val) {
-          //登陆成功保存session
-          Session::set('admin',$val[0]['USER']);
-          return array('msg'=>'登陆成功！','state'=>'1');  
+        $val=$this->LoginModel->select('SHOP_USER','USER,GNAME',"USER='{$request['user']}' AND PASSWORD='{$request['pwd']}'");
+        $actions = $this->LoginModel->select('SHOP_AUTH','AUTH',"NAME='{$val[0]['GNAME']}'");
+        if($val){
+        	if($val[0]['GNAME'] == 1){
+	        	Session::set('admin',1);
+	          	Session::set('actions',1);
+	        }elseif($val[0]['GNAME'] == 2){
+	        	Session::set('admin',2);
+	          	Session::set('actions',2);
+	        }else{
+	        	Session::set('admin',$val[0]['USER']);
+	          	Session::set('actions',$actions[0]['AUTH']);
+	        }
+	        return array('msg'=>'登陆成功！','state'=>'1');  
         }else{
-          return array('msg'=>'登陆失败！','state'=>'2');
+        	return array('msg'=>'登陆失败！','state'=>'2');
         }
     }
 
      //退出
      function exituser(){
         
-        if (Session::set('admin','')) {
+        if (Session::set('admin','')&&Session::set('actions','')) {
           //登陆成功保存session
           return array('msg'=>'退出成功！','state'=>'1');  
         }
